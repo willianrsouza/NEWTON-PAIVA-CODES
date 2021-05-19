45dbe725ca66eb3e953e23cb1615bfa4316ad662
Willian Rodrigues de Souza <willianrsouzawork@gmail.com>
2021-05-19 18:38:47 -0300

    Professor: Waldir Pires

---
+++ Code/Lab-B/DaoProfessor.java
@@ -0,0 +1,36 @@
+import java.util.List;
+import java.util.Optional;
+
+public class DaoProfessor implements Dao<Professor> {
+
+    @Override
+    public Optional<Professor> get(long id) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public List<Professor> getAll() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void save(Professor t) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void update(Professor t) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void delete(Professor t) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
---
+++ Code/Lab-B/Professor.java
@@ -0,0 +1,42 @@
+import java.sql.Date;
+
+public class Professor {
+
+    private Integer id;
+    private String matricula;
+    private String formacao;
+    private Date dataNasc;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getFormacao() {
+        return formacao;
+    }
+
+    public void setFormacao(String formacao) {
+        this.formacao = formacao;
+    }
+
+    public Date getDataNasc() {
+        return dataNasc;
+    }
+
+    public void setDataNasc(Date dataNasc) {
+        this.dataNasc = dataNasc;
+    }
+
+}
